32d041ce3563b10bc5291f67ef3296fb205938bd
Gustavo Roger Creutzberg <gustavo.creutzberg@gmail.com>
2020-02-23 19:16:34 -0300

    First commit ReactJS
    First commit ReactJS

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./styles.css";
+
+const Header = () => (
+    <header id="main-header">JSHunt</header>
+);
+
+/*
+function Header() {
+    return(
+        <h1>Hello</h1>
+    );
+}
+*/
+export default Header;
